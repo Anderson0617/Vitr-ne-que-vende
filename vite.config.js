c1f5975ec053c43b4800e0b131d1072dc348a04f
Anderson José Oliveira <anderson090485@hotmail.com>
2025-08-22 10:04:04 -0300

    Novo build corrigido

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/Vitr-ne-que-vende/',  // tem que ser exatamente igual ao nome do repositório no GitHub
+})
